afc79771489c0de8fe26c750f4598ee5c4f8e2a8
Shobhit Kushwaha <109756367+shokushwaha@users.noreply.github.com>
2022-10-01 19:08:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var weight, height, measure, bmi, error ;
+
+function calculate() {
+	weight = document.getElementById("weight").value;
+	height = document.getElementById("height").value;
+	error = "Please enter some values";
+	height /= 100;
+	height *= height;
+	bmi = weight/height;
+	bmi = bmi.toFixed(1);
+
+	if (bmi <= 18.4) {
+		measure = "Your BMI is " + bmi + " - - >  " + "you are Underweight  🤒";
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		measure = "Your BMI is " + bmi + " - - >  " + "You are Normal 😎 ";
+	} else if (bmi >= 25 && bmi <= 29.9) {
+		measure = "Your BMI is " + bmi + " - - >  " + "You are Overweight 😳 ";
+	} else if (bmi >= 30) {
+		measure = "Your BMI is " + bmi + " - - >  " + "You are Obese 🤥 ";
+	}
+	
+
+	if (weight === 0 ) {
+		document.getElementById("results").innerHTML = error;
+	} else if (height === 0){
+		document.getElementById("results").innerHTML = error;
+	}
+	 else {
+
+		document.getElementById("results").innerHTML = measure;
+	}
+	if (weight < 0) {
+		document.getElementById("results").innerHTML = "Negative Values not Allowed";
+	}
+}
